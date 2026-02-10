b10928710db8325ae9544b444be50d8aac6c6372
Ronin619 <eytam619@gmail.com>
2026-02-09 23:01:24 -0500

    update: add h1 tag to navbar

--- my_anime_repository/app/components/Navbar.js
+++ my_anime_repository/app/components/Navbar.js
@@ -2,17 +2,6 @@ export default function Navbar() {
   return (
     <nav className="navbar bg-primary navbar-expand-lg">
       <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
@@ -31,6 +20,7 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
+        <h1 className="text-light">My Anime Repository</h1>
       </div>
     </nav>
   );
