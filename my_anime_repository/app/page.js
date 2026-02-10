430bc8081c40916513e4e1849df09813c440ba1d
Ronin619 <eytam619@gmail.com>
2026-02-09 23:02:55 -0500

    add dispatch and useEffect of fetchTop10AnimeData

--- my_anime_repository/app/page.js
+++ my_anime_repository/app/page.js
@@ -1,9 +1,18 @@
 "use client";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Navbar from "./components/Navbar";
 import Top10AnimeCardGrid from "./components/Top10AnimeCardGrid";
+import { fetchTop10AnimeData } from "./store/slices/top10Anime";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTop10AnimeData());
+  }, [dispatch]);
+
   return (
     <>
       <Navbar />
