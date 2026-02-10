654bcba36e94327ae2214425a8689724059ab349
Ronin619 <eytam619@gmail.com>
2026-02-09 23:02:16 -0500

    update: image tag

--- my_anime_repository/app/components/Top10AnimeCardGrid.js
+++ my_anime_repository/app/components/Top10AnimeCardGrid.js
@@ -5,21 +5,20 @@ export default function Top10AnimeCardGrid() {
 
   return (
     <>
-      <div className="row row-cols-1 row-cols-md-5 g-4">
-        <h1>Top 10 Anime</h1>
+      <div className="row row-cols-1 row-cols-md-5 g-3">
         {top10AnimeData.map((anime) => (
-          <div className="col" key={anime.mal_id}>
+          <div className="col" key={anime.id}>
             <div className="card">
               <img
                 src={anime.image}
                 className="card-img-top"
-                alt="anime image"
+                alt={anime.title}
               />
               <div className="card-body">
                 <h5 className="card-title">{anime.title}</h5>
-                <p className="card-text">{anime.rank}</p>
-                <p className="card-text">{anime.episodes}</p>
-                <p className="card-text">{anime.year}</p>
+                <p className="card-text">Ranking: {anime.rank}</p>
+                <p className="card-text">Episodes: {anime.episodes}</p>
+                <p className="card-text">Year: {anime.year}</p>
               </div>
             </div>
           </div>
