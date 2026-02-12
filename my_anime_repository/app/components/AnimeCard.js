c3ca8134827386fbbff7f61b83d729a4050e012e
Ronin619 <eytam619@gmail.com>
2026-02-11 22:33:31 -0500

    feat: implement AnimeCard component

---
+++ my_anime_repository/app/components/AnimeCard.js
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+
+export default function AnimeCard({ searchedAnime }) {
+  if (!searchedAnime) return null;
+
+  return (
+    <div className="card mb-3 searched-Anime border border-3 border-white">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img
+            src={searchedAnime.images.jpg.image_url}
+            className="img-fluid rounded-start searched-img"
+            alt={searchedAnime.title}
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h4 className="card-title mb-1">{searchedAnime.title}</h4>
+            <p className="card-text mb-1">Rank: {searchedAnime.rank}</p>
+            {searchedAnime.year && (
+              <p className="card-text mb-1">Year: {searchedAnime.year}</p>
+            )}
+            <p className="card-text mb-4">Status: {searchedAnime.status}</p>
+            <button className="add-Btn mb-0">Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
