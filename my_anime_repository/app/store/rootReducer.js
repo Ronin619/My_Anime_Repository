0bbb79ce65964a1fc3f090a26d216ecdbbc5fd48
Ronin619 <eytam619@gmail.com>
2026-02-14 15:40:56 -0500

    update: rootReducer file with watchList reducer

--- my_anime_repository/app/store/rootReducer.js
+++ my_anime_repository/app/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import top10AnimeReducer from "./slices/top10Anime";
+import watchListReducer from "./slices/watchList";
 
 const rootReducer = combineReducers({
   top10Anime: top10AnimeReducer,
+  watchList: watchListReducer,
 });
 
 export default rootReducer;
