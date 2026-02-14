f86941c6978647e98193311b8571eb427453cf67
Ronin619 <eytam619@gmail.com>
2026-02-14 15:41:24 -0500

    update: remove home page link from navbar

--- my_anime_repository/app/components/Navbar.js
+++ my_anime_repository/app/components/Navbar.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="navbar bg-primary navbar-expand-lg">
@@ -5,18 +7,9 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a
-                className="nav-link active text-light"
-                aria-current="page"
-                href="#"
-              >
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-light" href="#">
+              <Link className="nav-link text-light" href="/post/watchList">
                 My Watch List
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
