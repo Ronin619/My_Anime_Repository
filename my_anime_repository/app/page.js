32bedb0c312acfe73ca9998e1f7227274416b770
Ronin619 <eytam619@gmail.com>
2026-02-14 15:43:10 -0500

    update: add watchListHandler function and pass as prop

--- my_anime_repository/app/page.js
+++ my_anime_repository/app/page.js
@@ -1,9 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "./components/Navbar";
 import Top10AnimeCardGrid from "./components/Top10AnimeCardGrid";
 import { fetchTop10AnimeData } from "./store/slices/top10Anime";
+import { addToWatchList } from "./store/slices/watchList";
 import AnimeSearcher from "./components/AnimeSearcher";
 import AnimeCard from "./components/AnimeCard";
 import axios from "axios";
@@ -11,6 +12,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   const [searchedAnime, setSearchedAnime] = useState(null);
+  const watchList = useSelector((state) => state.watchList.watchList);
 
   const dispatch = useDispatch();
 
@@ -31,16 +33,30 @@ export default function Home() {
     console.log(animeData);
   };
 
+  const watchListHandler = (anime) => {
+    const animeInfo = {
+      img: anime.image || anime.images.jpg.image_url,
+      title: anime.title,
+    };
+
+    if (!watchList.find((anime) => anime.title === animeInfo.title)) {
+      dispatch(addToWatchList(animeInfo));
+    }
+  };
+
   return (
     <>
       <Navbar />
       <div className="homePageBackground">
         <AnimeSearcher fetchAnime={fetchAnime} />
         <div className="searchedAnime">
-          <AnimeCard searchedAnime={searchedAnime} />
+          <AnimeCard
+            searchedAnime={searchedAnime}
+            watchListHandler={watchListHandler}
+          />
         </div>
         <div>
-          <Top10AnimeCardGrid />
+          <Top10AnimeCardGrid watchListHandler={watchListHandler} />
         </div>
       </div>
     </>
