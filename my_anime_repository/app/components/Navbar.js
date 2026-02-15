efc66941752e2d68f26ae4ad42cf787c31332db5
Ronin619 <eytam619@gmail.com>
2026-02-15 15:26:45 -0500

    update: navbar content

--- my_anime_repository/app/components/Navbar.js
+++ my_anime_repository/app/components/Navbar.js
@@ -1,15 +1,28 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export default function Navbar() {
+export default function Navbar({ pageTitle }) {
+  const pathname = usePathname();
+  const isWatchListPage = pathname === "/post/watchList";
+
   return (
     <nav className="navbar bg-primary navbar-expand-lg">
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link text-light" href="/post/watchList">
-                My Watch List
-              </Link>
+              {isWatchListPage ? (
+                <Link className="nav-link text-light fw-bold" href="/">
+                  Home
+                </Link>
+              ) : (
+                <Link
+                  className="nav-link text-light fw-bold"
+                  href="/post/watchList"
+                >
+                  My Watch List
+                </Link>
+              )}
             </li>
           </ul>
         </div>
