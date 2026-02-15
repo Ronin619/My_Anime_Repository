de023e77aaf26b8ed6a779413dd95eea26cc98cb
Ronin619 <eytam619@gmail.com>
2026-02-15 15:28:17 -0500

    update: watch list table

--- my_anime_repository/app/post/watchList/page.js
+++ my_anime_repository/app/post/watchList/page.js
@@ -1,13 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Navbar from "../../components/Navbar";
+import { deleteFromWatchList } from "../../store/slices/watchList";
 
 const WatchList = () => {
   const watchList = useSelector((state) => state.watchList.watchList);
+  const dispatch = useDispatch();
 
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <div className="watchList-container">
+        <Navbar />
+        <h1 className="watchList-title">Watch List</h1>
+        <div>
+          <table className="table table-bordered">
+            <tbody className="table-group-divider">
+              {watchList.map((anime, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>
+                    <img src={anime.img} alt={anime.title} width="50" />
+                  </td>
+                  <td className="anime-title">{anime.title}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger border-0"
+                      onClick={() => dispatch(deleteFromWatchList(anime.img))}
+                    >
+                      <i className="bi bi-trash3-fill"></i>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 };
 
